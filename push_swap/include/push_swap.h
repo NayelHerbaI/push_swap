82a3074d550d1bd579606f9922c216e7e1bf79b9
Herbal Nayel <hnayel@f4r11s8.clusters.42paris.fr>
2026-01-29 17:59:05 +0100

    okaaaay

--- push_swap/include/push_swap.h
+++ push_swap/include/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: hnayel <hnayel@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/18 17:52:56 by naherbal          #+#    #+#             */
-/*   Updated: 2026/01/29 17:00:59 by hnayel           ###   ########.fr       */
+/*   Updated: 2026/01/29 17:58:33 by hnayel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -78,20 +78,19 @@ int					smaller_and_bigger_number_pos(t_data *data, char c);
 void				zero_one_pos(t_data *data, int small);
 void				three_four_pos(t_data *data, int small);
 void				kyle_function(t_data *data, int small);
-
-void	sort_four(t_data *data);
-void	sort_three(t_data *data);
-int	find_max(t_node *node);
-int	find_min(t_node *node);
-void	put_first_to_last(t_node **list);
-void	swap_two_first(t_node *list);
-void	add_front(t_node **list, t_node *new);
-void	pick_sort(t_data *data);
-void	put_min_max_up(t_data *data);
-void	sort_five(t_data *data);
-void	put_last_to_first(t_node **list);
-void	push_a_to_b(t_data *data);
-void	push_b_to_a(t_data *data);
-int	is_sorted(t_node *a);
+void				sort_four(t_data *data);
+void				sort_three(t_data *data);
+void				put_first_to_last(t_node **list);
+void				swap_two_first(t_node *list);
+void				add_front(t_node **list, t_node *new);
+void				pick_sort(t_data *data);
+void				put_min_max_up(t_data *data);
+void				sort_five(t_data *data);
+void				put_last_to_first(t_node **list);
+void				push_a_to_b(t_data *data);
+void				push_b_to_a(t_data *data);
+int					find_max(t_node *node);
+int					find_min(t_node *node);
+int					is_sorted(t_node *a);
 
 #endif
